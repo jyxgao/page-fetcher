3486350615bc61c1bd1499bef742d668efd5b527
Jenny Gao <gao.jenny9@gmail.com>
2020-07-30 05:42:50 +0000

    added stretch: checks if content exists before writing and gives warning

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
+const { createBrotliCompress } = require('zlib');
 
 const url = process.argv[2];
 const path = process.argv[3];
@@ -15,21 +16,43 @@ request(url, (error, response, body) => {
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body);
 
-  // // if(   ****  ) {
-  //   rl.question("File already exists, type Y to overwrite", (Yes) => {
-  //     if (Yes === "Y"){
-  //       // fetch the body from the request above, write it into path 
+
+    fs.readFile(path, 'utf8', (err, data) => {
+      let content = "";
+      if (err) { 
+        cb(err) 
+      }
+      content = cb(null, data)
+      console.log(content)
+
+      if (content.length > 0) {
+        rl.question("File already exists, type Y to overwrite", (Yes) => {
+          if (Yes === "Y"){
+            // fetch the body from the request above, write it into path 
+            fs.writeFile(path, body, (err) => {
+              if (err) {         
+                console.log('Failed to write to localPath: ', path);
+              } else {
+                console.log(`Downloaded and saved ${body.length} bytes to ${path}`); 
+              }
+            });
+            rl.close()
+          }
+        })
+      } else {
         fs.writeFile(path, body, (err) => {
           if (err) {         
             console.log('Failed to write to localPath: ', path);
           } else {
             console.log(`Downloaded and saved ${body.length} bytes to ${path}`); 
           }
-  //     }
-  //     rl.close()
-  //   })
-  // }
-  });
+        })
+      }
+    })
 });
 
 
+const cb = function(err, data) {
+  if (err) { throw err };
+  return data;
+}
